a08afd02f70cdfad09a0cc7ea86e1db24eb43c73
JiahaoLi <Jiahao.Li@feisystems.com>
2016-02-04 14:59:15 -0500

    Create a new Spring Boot application for DSS

---
+++ dss/src/main/java/gov/samhsa/mhc/dss/service/DssApplication.java
@@ -0,0 +1,12 @@
+package gov.samhsa.mhc.dss.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DssApplication.class, args);
+	}
+}
