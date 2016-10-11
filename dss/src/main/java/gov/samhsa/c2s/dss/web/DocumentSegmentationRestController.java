c62b6028282fa5cdc1efe30dd37d9613c7fe8470
Shruti Rao <shruti.rao@feisystems.com>
2016-10-11 12:40:34 -0400

    Optimize imports, code cleanup and reformat code
    #11132:As a developer, I will have completed CLEAN-UP of 5-9 of 13 Git backend code repositories to make it more deployable by community member (HIE)

--- dss/src/main/java/gov/samhsa/c2s/dss/web/DocumentSegmentationRestController.java
+++ dss/src/main/java/gov/samhsa/c2s/dss/web/DocumentSegmentationRestController.java
@@ -1,9 +1,9 @@
 package gov.samhsa.c2s.dss.web;
 
 import ch.qos.logback.audit.AuditException;
-import gov.samhsa.c2s.dss.service.DocumentSegmentation;
 import gov.samhsa.c2s.common.validation.exception.XmlDocumentReadFailureException;
 import gov.samhsa.c2s.dss.service.ClinicalDocumentValidation;
+import gov.samhsa.c2s.dss.service.DocumentSegmentation;
 import gov.samhsa.c2s.dss.service.dto.ClinicalDocumentValidationRequest;
 import gov.samhsa.c2s.dss.service.dto.ClinicalDocumentValidationResult;
 import gov.samhsa.c2s.dss.service.dto.DSSRequest;
