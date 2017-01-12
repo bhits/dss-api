56080387eabd198ceb3a9fef95670a7b41f06edd
JiahaoLi <Jiahao.Li@feisystems.com>
2017-01-11 22:55:12 -0500

    Add initial configuration to DSS to work with config-server
    #12361 - As a developer, I can use a centralized configuration management tool

--- dss/src/test/java/gov/samhsa/c2s/dss/DssApplicationTests.java
+++ dss/src/test/java/gov/samhsa/c2s/dss/DssApplicationTests.java
@@ -3,16 +3,15 @@ package gov.samhsa.c2s.dss;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = DssApplication.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Ignore("Depends on config-server on bootstrap")
 public class DssApplicationTests {
 
     @Test
-    @Ignore
     public void contextLoads() {
     }
-
 }
