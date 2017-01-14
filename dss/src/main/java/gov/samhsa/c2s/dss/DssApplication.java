59834d6858a3df9cc6d5e152b3a8a845c77af7b7
JiahaoLi <Jiahao.Li@feisystems.com>
2017-01-13 23:49:06 -0500

    Enable security scope for accessing management endpoint
    #12361 - As a developer, I can use a centralized configuration management tool

--- dss/src/main/java/gov/samhsa/c2s/dss/DssApplication.java
+++ dss/src/main/java/gov/samhsa/c2s/dss/DssApplication.java
@@ -6,11 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @ComponentScan(basePackageClasses = {DssApplication.class, BrmsBasePackageMarkerInterface.class})
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableResourceServer
 public class DssApplication {
 
     public static void main(String[] args) {
