07cd0748518be3a7f798e0fc566dcb894752e556
burcakulug <burcak.ulug@feisystems.com>
2017-01-19 16:01:15 -0500

    Set test web environment to use random port
    #12361:As a developer, I can use a centralized configuration management tool, in order to enhance continuous delivery of C2S and the development process (part2)

--- dss/src/test/java/gov/samhsa/c2s/dss/DssApplicationTests.java
+++ dss/src/test/java/gov/samhsa/c2s/dss/DssApplicationTests.java
@@ -4,13 +4,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("default")
-@TestPropertySource(properties = {"port: 0"})
 public class DssApplicationTests {
 
     @Test
